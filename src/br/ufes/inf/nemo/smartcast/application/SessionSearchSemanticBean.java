0dfd13b883cd42ae9b4c3e5e2714a553bc030022
Vítor E. Silva Souza <vitorsouza@gmail.com>
2017-07-31 16:40:52 -0300

    Fix small bug (implements -> extends)

--- src/br/ufes/inf/nemo/smartcast/application/SessionSearchSemanticBean.java
+++ src/br/ufes/inf/nemo/smartcast/application/SessionSearchSemanticBean.java
@@ -20,7 +20,7 @@ import br.ufes.inf.nemo.smartcast.persistence.PodcastDAO;
 @SessionScoped
 @Stateful
 
-public class SessionSearchSemanticBean implements SessionSearchSemantic{
+public class SessionSearchSemanticBean extends SessionSearchSemantic{
 	/** Serialization id. */
 	private static final long serialVersionUID = 1L;
 	
